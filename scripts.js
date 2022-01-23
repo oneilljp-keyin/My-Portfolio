0a0870e942858f67cee6317b411a2e7a0b24bdde
oneilljp-keyin <john.oneill@keyin.com>
2022-01-23 19:01:23 -0330

    add resume link, tweaked mobile look

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const parallax = document.querySelector("#grid");
 
 function parallaxScroll() {
-    window.addEventListener("scroll", function() {
-        let offset = window.pageYOffset;
-        document.body.style.backgroundPositionY = (offset * -0.5) + "px";
-    });
+  window.addEventListener("scroll", function () {
+    let offset = window.pageYOffset;
+    document.body.style.backgroundPositionY = offset * -0.5 + "px";
+  });
 }
 
 window.addEventListener("load", parallaxScroll);
@@ -13,9 +13,9 @@ window.addEventListener("load", parallaxScroll);
 mybutton = document.querySelector("#myBtn");
 
 // When the user scrolls down 20px from the top of the window, show the button
-document.querySelector('.main').addEventListener("scroll", scrollFunction);
+document.querySelector(".main").addEventListener("scroll", scrollFunction);
 function scrollFunction() {
-  if (document.querySelector('.main').scrollTop > 20) {
+  if (document.querySelector(".main").scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -24,13 +24,22 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the window
 function topFunction() {
-  document.querySelector('.main').scrollTop = 0;
+  document.querySelector(".main").scrollTop = 0;
 }
 
-
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 for (const card of cards) {
-  card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
+  card.addEventListener("click", function () {
+    card.classList.toggle("is-flipped");
+  });
 }
+
+// Hide Menu on click
+// function hideMenu() {
+//   let x = document.querySelector(".dropdown-content");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
